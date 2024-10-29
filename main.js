c0a811c90fed7f7207e27d7db15c63c6b255a2ca
ariflabs <arifaalfatih@gmail.com>
2024-10-29 14:43:01 +0700

    add sample product  and cart content

--- main.js
+++ main.js
@@ -1,10 +1,34 @@
 const app = Vue.createApp({
     data: function() {
         return {
-            msg: 'signal from main.js',
-            desc: '...Kni...knife...fe',
-            sampleImg: './assets/coffee-machine.png',
-            lists: ['Help','sos','lend']
+            cart: 0,
+            product: 'Coffee Machine',
+            brand:'V-comerce',
+            selectedVariant: 0,
+            details: ['with grinder', 'espresso maker', 'steam milk'],
+            variants: [
+                { id: 1123, color: 'black', image: './assets/coffee-machine-black.jpg', quantity: 30 },
+                { id: 1124, color: 'white', image: './assets/coffee-machine-white.jpg', quantity: 0 }
+            ]
+        }
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        updateVariant(index) {
+            this.selectedVariant = index
+        }
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product
+        },
+        image() {
+            return this.variants[this.selectedVariant].image
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].quantity
         }
     }
 });
